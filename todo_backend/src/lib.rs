3c176eb95058eecf86eb36006bb9189186acd9db
Sameer2748 <mrao27488@gmail.com>
2026-02-02 17:01:18 +0530

    Refactor app structure and add integration testing setup

--- todo_backend/src/lib.rs
+++ todo_backend/src/lib.rs
@@ -1,5 +1,30 @@
+use axum::{Router, middleware, routing::{delete, get, post, put}};
+
+use crate::{app_state::AppState, checker::mid::auth_middleware, db::connection::connect_db, handlers::{todo, user}};
+
 pub mod app_state;
 pub mod db;
 pub mod handlers;
 pub mod auth;
 pub mod checker;
+
+
+pub async fn create_router()-> Router{
+     let pool = connect_db().await;
+    let state = AppState { pool };
+    println!("connected db successfully ");
+    let protected_routes = Router::new()
+    .route("/getall", get(todo::getalltodo))
+    .route("/create", post(todo::create_Todo))
+    .route("/todo/:id", get(todo::getTodo))
+    .route("/todo/:id", delete(todo::deleteTodo))
+    .route("/todo/:id", put(todo::updateTodo))
+    .layer(middleware::from_fn(auth_middleware)).with_state(state.clone()); // layer help us add the middleware trait in this router and in that we ahve from_fn which take our middleware funtion 
+
+    Router::new()
+        .route("/", get(|| async {"hello from backend"}))
+        .route("/signin", post(user::signinuser))
+        .route("/signup", post(user::signupuser))
+        .merge(protected_routes)
+        .with_state(state)
+}
--- todo_backend/src/main.rs
+++ todo_backend/src/main.rs
@@ -1,42 +1,10 @@
-use axum::{
-    routing::{get, post, delete, put},
-    Router,
-    middleware
-};
 
-mod handlers;
-mod db;
-mod app_state;
-mod auth;
-mod checker;
-
-
-use handlers::{user, todo};
-use app_state::AppState;
-use db::connection::connect_db;
-use checker::mid::auth_middleware;
+use todo_backend::create_router; 
 
 #[tokio::main]
-async fn main(){
-    let pool = connect_db().await;
-    let state = AppState { pool };
-    println!("connected db successfully ");
-    
-    let protected_routes = Router::new()
-    .route("/getall", get(todo::getalltodo))
-    .route("/create", post(todo::create_Todo))
-    .route("/todo/:id", get(todo::getTodo))
-    .route("/todo/:id", delete(todo::deleteTodo))
-    .route("/todo/:id", put(todo::updateTodo))
-    .layer(middleware::from_fn(auth_middleware)).with_state(state.clone()); // layer help us add the middleware trait in this router and in that we ahve from_fn which take our middleware funtion 
+async fn main() {
+    let app = create_router().await;
 
-    let app = Router::new()
-        .route("/", get(|| async {"hello from backend"}))
-        .route("/signin", post(user::signinuser))
-        .route("/signup", post(user::signupuser))
-        .merge(protected_routes)
-        .with_state(state);
-    
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
